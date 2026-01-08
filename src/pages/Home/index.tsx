43528632424325add03b1e0e888c6812ae575a91
肖振洋 <xiaozhenyang@huomakeji.com>
2026-01-08 14:06:41 +0800

    feat: 修改提交信息

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,11 @@
-import { Icon } from "@iconify/react"
-import { useBoolean } from "ahooks";
-import type { FC } from "react"
+import { Icon } from '@iconify/react'
+import type { FC } from 'react'
 
 export const Home: FC = () => {
-  const [value, toggle] = useBoolean(false);
   return (
     <div className="w-[300px] h-[200px] m-auto text-3xl flex flex-col items-center">
-      <h1 className='text-red-600'>react-ts-template</h1>
+      <h1 className="text-red-600">react-ts-template</h1>
       <Icon icon="mdi:home" width="48" height="48" color="red" />
-      <div className="text-orange-300 cursor-pointer" onClick={toggle.toggle}>{value ? "true" : "false"}</div>
     </div>
   )
 }
